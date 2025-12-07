bf36150a01a09cd4a3460a983528a9c6737d208b
geffrys <geffry.ospina@gmail.com>
2025-12-07 15:32:03 -0500

    archivos faltantes del commit anterior

---
+++ backend/stoicron-back/src/main/java/com/stoicron/stoicron_back/config/exception/NoUserFoundException.java
@@ -0,0 +1,7 @@
+package com.stoicron.stoicron_back.config.exception;
+
+public class NoUserFoundException extends Exception {
+    public NoUserFoundException(String message) {
+        super(message);
+    }
+}
