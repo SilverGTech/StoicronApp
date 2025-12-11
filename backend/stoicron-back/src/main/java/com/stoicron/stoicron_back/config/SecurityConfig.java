8dad3aef0621b65c67306af5322fe1fe5d3732cb
geffrys <geffry.ospina@gmail.com>
2025-12-11 01:13:58 -0500

    now code is working and well documented

--- backend/stoicron-back/src/main/java/com/stoicron/stoicron_back/config/SecurityConfig.java
+++ backend/stoicron-back/src/main/java/com/stoicron/stoicron_back/config/SecurityConfig.java
@@ -2,6 +2,7 @@ package com.stoicron.stoicron_back.config;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Bean;
+import org.springframework.http.HttpHeaders;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.AuthenticationProvider;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
@@ -10,14 +11,18 @@ import org.springframework.security.config.annotation.method.configuration.Enabl
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
+import com.stoicron.stoicron_back.auth.model.AuthToken;
 import com.stoicron.stoicron_back.auth.model.AuthUser;
+import com.stoicron.stoicron_back.auth.repository.AuthTokenRepository;
 import com.stoicron.stoicron_back.auth.repository.AuthUserRepository;
+import com.stoicron.stoicron_back.auth.service.AuthTokenService;
 import com.stoicron.stoicron_back.config.exception.NoUserFoundException;
 import com.stoicron.stoicron_back.config.utils.Errors;
 
@@ -32,9 +37,8 @@ import lombok.extern.log4j.Log4j2;
 @Log4j2
 public class SecurityConfig {
 
-    private final JwtAuthFilter jwtAuthFilter;
-
     private final AuthUserRepository authUserRepository;
+    private final AuthTokenRepository authTokenRepository;
 
 
     @Bean
@@ -72,14 +76,46 @@ public class SecurityConfig {
                             response.setContentType("application/json");
                             response.getWriter().write("{\"error\":\"" + authException.getMessage() + "\"}");
                         }))
-                        .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
-                        .authenticationProvider(authenticationProvider(userDetailsService()))
-                        .addFilterBefore(jwtAuthFilter, UsernamePasswordAuthenticationFilter.class);
+                .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                .authenticationProvider(authenticationProvider(userDetailsService()))
+                .addFilterBefore(jwtAuthFilter(), UsernamePasswordAuthenticationFilter.class)
+                .logout(logout -> logout
+                        .logoutUrl("/auth/logout")
+                        .addLogoutHandler((request, response, authentication) -> {
+                            final var authHeader = request.getHeader(HttpHeaders.AUTHORIZATION);
+                            logout(authHeader);
+                        })
+                        .logoutSuccessHandler((request, response, authentication) -> {
+                            SecurityContextHolder.clearContext();
+                        }));
         return http.build();
+    }
+
+    private void logout(String authHeader) {
+        if (authHeader == null || !authHeader.startsWith("Bearer ")) {
+            return;
+        }
+        final String jwt = authHeader.substring(7);
+        final AuthToken authToken = authTokenRepository.findByToken(jwt);
+        if (authToken != null) {
+            authToken.setRevoked(true);
+            authToken.setExpired(true);
+            authTokenRepository.save(authToken);
+        }
     }
 
     @Bean
     public PasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public JwtAuthFilter jwtAuthFilter() {
+        return new JwtAuthFilter(authTokenService(), userDetailsService(), authTokenRepository, authUserRepository);
+    }
+
+    @Bean
+    public AuthTokenService authTokenService() {
+        return new AuthTokenService(authTokenRepository);
     }
 }
